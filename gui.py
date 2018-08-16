036747e48afa6f0d37af20b635830ba5aaeb2be1
hsm-hx <skytea922@gmail.com>
2018-08-16 21:53:34 +0900

    :heavy_plus_sign: QRコードのデコード処理まで

---
+++ gui.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+# GUI
+from tkinter import *
+
+# 画像処理
+from PIL import Image
+from pyzbar import pyzbar
+
+def QRpushed():
+    qrimg = Image.open('qrsample.png')
+    strings = pyzbar.decode(qrimg)
+    print(strings)
+    
+def init():
+    root = Tk()
+    root.title("Procon29 solver")
+    root.geometry("640x480")
+
+    button = Button(
+            root, 
+            text="read QRcode", 
+            command = QRpushed
+            )
+    button.pack()
+
+    return root
+
+if __name__ == "__main__":
+    root = init()
+    root.mainloop()
