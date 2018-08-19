5ca33c78c61b753a235a6ee165a925b9ce051f46
hsm-hx <skytea922@gmail.com>
2018-08-19 22:31:03 +0900

    :heavy_plus_sign: #1 フィールドデータをオブジェクトに展開

--- gui/gui.py
+++ gui/gui.py
@@ -12,14 +12,39 @@ class Square:
     state = FREE
     point = 0;
     
-    def __init__():
-        pass
+    def __init__(self, value):
+        self.setPoint(value)
+
+    # getter
+    def getPoint(self):
+        return self.point
+    def getState(self):
+        return self.state
+
+    # setter
+    def setPoint(self, value):
+        self.point = value
+    def setState(self, value):
+        self.state = value
 
 class Field:
+    height = 12; width = 12
     sqs = [[0 for i in range(12)] for j in range(12)] 
-    def __init__():
-        pass
+    def __init__(self, data):
+        self.genField(data)
+
+    def genField(self, data):
+        self.height = data[0][0]; self.width = data[0][1]
+        for i in range(self.height):
+            for j in range(self.width):
+                self.sqs[i][j] = Square(data[i+1][j])
         
+    def showFieldList(self):
+        for i in range(self.height):
+            for j in range(self.width):
+                print(str("{0:2d}".format(self.sqs[i][j].getPoint())) + " ", end="")
+            print("\n")
+
 def QRpushed():
     qrimg = Image.open('../img/qrsample.png')
     fdata = pyzbar.decode(qrimg)[0][0].decode('utf-8')
@@ -31,11 +56,11 @@ def qrDataParse(qdata):
     data.remove("")
     for i in range(len(data)):
         data[i] = list(map(int, data[i].split()))
-    print(data)
+    return data
 
 def fieldGenerate(data):
-    qrDataParse(data)
-    #field = Field()
+    field = Field(qrDataParse(data))
+    field.showFieldList()
     
 def init():
     root = Tk()
