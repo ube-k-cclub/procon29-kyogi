bc9e24b9ca24a7322a1cdec080a6a8c1f78e9e7b
hsm-hx <skytea922@gmail.com>
2018-10-16 23:57:47 +0900

    :heavy_plus_sign: エージェントの追跡を実装

--- gui/gui.py
+++ gui/gui.py
@@ -12,56 +12,106 @@ from pyzbar import pyzbar
 # クラス定義
 # ------------------------------
 FREE, OWN, ENEMY = 0, 1, 2
-mode = OWN
 
 class Square:
+    label = None
     _state = FREE
     _point = 0
-    label = None
+    _player = 0
+    _field = None
     
-    def __init__(self, value, master):
+    def __init__(self, value, master, field):
         self.setPoint(value)
+        self.setField(field)
         self.label = Label(
             master,
             text = str(self.getPoint()),
-            foreground = "#ffffff",
+            foreground = self.getFg(),
             background = self.getColor(),
+            borderwidth = 3,
+            relief = "ridge",
             font = ("Migu 1C", 18),
             width = 2,
-            padding = (5, 5)
+            padding = (2, 2)
             )
-        self.label.bind("<1>", self.__clicked)
+        self.label.bind("<1>", self.__Lclicked)
+        self.label.bind("<3>", self.__Rclicked)
 
-    def __clicked(self, event):
+    # 左クリックで自陣操作
+    def __Lclicked(self, event):
+        # エージェントのいる位置を初めてクリックしたとき
+        if self.getState() == OWN and self.getField().clicked == -1:
+            # クリックされたマスを保存
+            self.getField().clicked = self
+        # 既にエージェントがクリックされた後の場合
+        elif self.getField().clicked != -1:
+            # 移動先が自陣
+            if self.getState() == OWN:
+                self.setFg(self.getField().clicked.getPlayer())
+                self.getField().clicked.setFg(0)
+                self.getField().clicked = -1
+            # 指定先が敵陣
+            elif self.getState() == ENEMY:
+                self.setState(FREE)
+                self.getField().clicked = -1
+            # 指定先が空白
+            else:
+                self.setState(OWN)
+                self.setFg(self.getField().clicked.getPlayer())
+                self.getField().clicked.setFg(0)
+                self.getField().clicked = -1
+
+    def __Rclicked(self, event):
         if self.getState() == FREE:
-            self.setState(mode)
+            self.setState(ENEMY)
+            field = self.getField()
+
         else:
             self.setState(FREE)
 
-
     # getter
+    def getField(self):
+        return self._field
     def getPoint(self):
         return self._point
     def getState(self):
         return self._state
     def getColor(self):
         if self._state == FREE:
-            return "#161616"
+            return "#202020"
         elif self._state == OWN:
-            return "#87ceeb"
+            return "#1F6764"
         elif self._state == ENEMY:
-            return "#ffb6c1"
+            return "#C94D65"
+    def getPlayer(self):
+        return self._player
+    def getFg(self):
+        if self._player == 1:
+            return "#888888"
+        elif self._player == 2:
+            return "#F58888"
+        else:
+            return "#ffffff"
 
     # setter
+    def setField(self, value):
+        self._field = value
     def setPoint(self, value):
         self._point = value
     def setState(self, value):
         self._state = value
         self.label["background"] = self.getColor()
+    def setFg(self, value):
+        self._player = value
+        self.label["foreground"] = self.getFg()
 
 class Field:
     height, width = 12, 12
     sqs = [[0 for i in range(12)] for j in range(12)] 
+
+    pown = [None, None]
+    pene = [None, None]
+    clicked = -1
     
     def __init__(self, data, master):
         self.genField(data, master)
@@ -71,15 +121,20 @@ class Field:
 
         for i in range(self.height):
             for j in range(self.width):
-                self.sqs[i][j] = Square(data[i+1][j], master)
+                self.sqs[i][j] = Square(data[i+1][j], master, self)
         
         # 自エージェントの初期位置を取得
         sdefault = [[data[self.height+1][0], data[self.height+1][1]], \
                      [data[self.height+2][0], data[self.height+2][1]]]
 
-        # 両チームの初期陣地を設定
-        self.sqs[sdefault[0][0] - 1][sdefault[0][1] - 1].setState(OWN)
-        self.sqs[sdefault[1][0] - 1][sdefault[1][1] - 1].setState(OWN)
+        # 初期陣地を設定
+        self.pown[0] = self.sqs[sdefault[0][0] - 1][sdefault[0][1] - 1]
+        self.pown[0].setState(OWN)
+        self.pown[0].setFg(1)
+        self.pown[1] = self.sqs[sdefault[1][0] - 1][sdefault[1][1] - 1]
+        self.pown[1].setState(OWN)
+        self.pown[1].player = 1
+        self.pown[1].setFg(2)
 
 # ------------------------------
 # 関数定義 
@@ -121,9 +176,6 @@ class FieldFrame(Frame):
     def __init__(self, master = None):
         self.master = master
         Frame.__init__(self, master)
-        self['height'] = 480
-        self['width'] = 640
-        # self['background'] = "#161616"
         self.grid(row=0, column=0)
         self.init()
 
@@ -133,30 +185,26 @@ class FieldFrame(Frame):
             for j in range(self.field.width):
                 self.field.sqs[i][j].label.grid(row=i+1, column=j+1)
 
-class SwitchFrame(Frame):
-    modeLabel = None
+class DetailFrame(Frame):
+    turn = None
 
-    def __initWidget(self):
-        self.modeLabel = Label(
+    def init(self):
+        self.turn = Label(
             self,
-            text = str(mode),
+            text="40/40TURN",
             )
-        self.modeLabel.bind("<1>", self.__changeMode)
-        self.modeLabel.grid(row=0, column=0)
+        self.turn.grid(row=0, column=0)
 
+    def __init__(self, master = None):
+        self.master = master
+        Frame.__init__(self, master)
+        self.grid(row=0, column=0)
+        self.init()
+
+class SwitchFrame(Frame):
     def __init__(self, master = None):
         Frame.__init__(self, master)
         self.grid(row=0, column=0)
-        self.__initWidget()
-
-    def __changeMode(self, event):
-        global mode
-        if mode == OWN:
-            mode = ENEMY  
-        else:
-            mode = OWN
-
-        self.modeLabel["text"] = mode
 
 class QRButtonFrame(Frame):
     nextfrm = None
@@ -195,13 +243,16 @@ def main():
     style.configure(".", foreground = "#ffffff")
     style.configure(".", background = "#161616")
 
-    swtfrm = FieldFrame(root)
-    swtfrm.grid(row=1, column=1)
+    fldfrm = FieldFrame(root)
+    fldfrm.grid(row=1, column=1)
 
-    modfrm = SwitchFrame(root)
-    modfrm.grid(row=1, column=0)
+    swtfrm = SwitchFrame(root)
+    swtfrm.grid(row=1, column=0)
+    
+    dtlfrm = DetailFrame(root)
+    dtlfrm.grid(row=0, column=0)
 
-    showQRwindow(swtfrm)
+    showQRwindow(fldfrm)
     
     field = None
 
