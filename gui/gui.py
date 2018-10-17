8ed1d0212e8f2dbfaf133b9b6ba6b6ec46789880
hsm-hx <skytea922@gmail.com>
2018-10-18 00:00:42 +0900

    :heavy_plus_sign: webcamからのQRコード読み取りを実装

--- gui/gui.py
+++ gui/gui.py
@@ -7,6 +7,10 @@ from tkinter.ttk import *
 # 画像処理
 from PIL import Image
 from pyzbar import pyzbar
+import cv2
+
+# その他
+import numpy as np
 
 # ------------------------------
 # クラス定義
@@ -158,6 +162,15 @@ def showQRwindow(swtfrm):
     btnfrm = QRButtonFrame(swtfrm, subWindow)
     btnfrm.grid(row=0, column=0)
 
+def edit_contrast(image, gamma):
+    look_up_table = [np.uint8(255.0 / (1 + np.exp(-gamma * (i - 128.) / 255.)))
+        for i in range(256)]
+ 
+    result_image = np.array([look_up_table[value]
+                             for value in image.flat], dtype=np.uint8)
+    result_image = result_image.reshape(image.shape)
+    return result_image
+ 
 # ------------------------------
 # フレーム定義
 # ------------------------------
@@ -226,10 +239,27 @@ class QRButtonFrame(Frame):
         self.init()
 
     def pushed(self):
-        qrimg = Image.open('../img/qrsample.png')
-        fdata = pyzbar.decode(qrimg)[0][0].decode('utf-8')
-        self.nextfrm.field = Field(dataParse(fdata), self.nextfrm)
-        self.master.withdraw()
+        capture = cv2.VideoCapture(0)
+        if capture.isOpened() is False:
+            raise("IO Error")
+     
+        while True:
+            ret, frame = capture.read()
+            if ret == False:
+                continue
+            
+            # グレースケール化してコントラストを調整する
+            gray_scale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            image = edit_contrast(gray_scale, 5)
+     
+            # 加工した画像からフレームQRコードを取得してデコードする
+            codes = pyzbar.decode(image)
+     
+            if len(codes) > 0:
+                fdata = codes[0][0].decode('utf-8')
+                self.nextfrm.field = Field(dataParse(fdata), self.nextfrm)
+                self.master.withdraw()
+                break
 
 def main():
     root = Tk()
